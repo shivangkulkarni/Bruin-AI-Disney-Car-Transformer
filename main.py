d7495af7f8893e306fef249f978bd517ea2a0450
Yifan Zhou <yifan.zhou11882-bigz@basischina.com>
2024-10-22 08:10:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from transformers import BlipProcessor, BlipForConditionalGeneration
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
 
-image_path = 'image.jpg' 
+image_path = 'image.jpg' # Image path
 raw_image = Image.open(image_path).convert('RGB')
 
 # image captioning
